e0f61d1eeb68a08d21c50db31b904b0e65f9a44b
Chanuk Kim <cksdnr987@gmail.com>
2021-12-03 11:25:09 +0900

    add direnv-switch.sh

---
+++ direnv-switch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# https://github.com/direnv/direnv/issues/544 
+# Usage: direnv-switch <env-name>
+set -euo pipefail
+
+# take the env as the first argument
+env_name=$1
+env_file=.envrc.${env_name}
+
+# check if the .env.$env_name exists
+if [[ ! -f $env_file ]]; then
+  echo "env $env_name not found" >&2
+  exit 1
+fi
+
+# create a new .envrc for the user
+cat <<NEW_ENVRC > .envrc
+log Loading environment "$env_name"
+dotenv "$env_file"
+NEW_ENVRC
+# allow the execution
+direnv allow .
